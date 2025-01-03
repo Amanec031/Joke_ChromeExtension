ab128eba3dbb26b7df8c4d25aa1145c9a46ff8e2
Amanec031 <amankumarmenu@gmail.com>
2025-01-04 01:10:24 +0530

    updated script file

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-fetch('https://icanhazdadjoke.com/slack')
-.then(data => data.json())
-.then(jokeData =>{
-    const jokeText = jokeData.attachments[0].text;
-    const jokeElement = document.getElementById('jokeElement');
-    jokeElement.innerHTML = jokeText;
-})
+const jokeElement = document.getElementById('jokeElement');
+const newJokeButton = document.getElementById('newJokeButton');
+const loader = document.getElementById('loader');
+
+ function fetchJoke() {
+    showLoader();
+    fetch('https://icanhazdadjoke.com/slack')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch joke');
+            }
+            return response.json();
+        })
+        .then(jokeData => {
+            const jokeText = jokeData.attachments[0].text;
+            jokeElement.textContent = jokeText;
+            hideLoader();
+        })
+        .catch(error => {
+            jokeElement.textContent = "Oops! Couldn't fetch a joke. Try again later.";
+            console.error(error);
+            hideLoader();
+        });
+}
+
+ function showLoader() {
+    loader.classList.remove('hidden');
+    jokeElement.textContent = '';
+}
+
+ function hideLoader() {
+    loader.classList.add('hidden');
+}
+
+ newJokeButton.addEventListener('click', fetchJoke);
+
+ fetchJoke();
